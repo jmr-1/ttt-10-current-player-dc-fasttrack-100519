b703fe3a1176af43ad0b56a19f8fe56bf32b6a00
J <jmrumingan@gmail.com>
2019-10-06 15:28:29 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,11 +1,14 @@
 def turn_count(board)
-  counter = 0;
-  
+  counter = 1;
+  puts counter;
   board.length.times do |num|
-    puts board[num]
-    prints board 
+    puts board[num];
+    print board;
+    if board[num] =="X" || board[num] =="O"
+      counter = counter + 1;
+    end 
   end 
-  
+  puts counter;
 end
 
 def current_player
